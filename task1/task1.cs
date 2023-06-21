93cbdcab8ac37ff62835a739393da444c06751a4
Name <vkonohov19@gmail.com>
2023-06-21 17:34:38 +0300

    update

---
+++ task1/task1.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int chislo(string message)
+{
+  Console.Write(message);
+  int result = int.Parse(Console.ReadLine()!);
+  return result;
+}
+
+int NaturalNumber(int n, int m)
+{
+  if (n == m) return n;
+  else 
+  Console.Write($"{NaturalNumber(n, m + 1)}, ");
+  return m;
+}
+
+int n = chislo("Введите натуральное число: ");
+if (n < 1)
+{
+  Console.WriteLine("Введите положительное число!");
+  return;
+}
+Console.WriteLine(NaturalNumber(n, 1));
+
