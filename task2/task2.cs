88f8fe24e8489de7d0b6685b7ff69458eed6cbbb
Name <vkonohov19@gmail.com>
2023-06-21 17:43:25 +0300

    update

---
+++ task2/task2.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Введите число M (больше N): ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N (Меньше M): ");
+int n = int.Parse(Console.ReadLine()!);
+
+Sum(m, n);
+
+void Sum(int m, int n)
+{
+    Console.Write(Summa(m - 1, n));
+}
+
+int Summa(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + Summa(m, n);
+        return res;
+    }
+}
